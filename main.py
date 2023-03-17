1f4a098239c55ccf64e5f0c2f72bb3c12c457cd9
Ofem Eteng <ofemeteng@gmail.com>
2023-03-17 09:49:38 +0100

    gigza backend: add CORS middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 from utils import crud, models, schemas
@@ -10,6 +11,20 @@ models.Base.metadata.create_all(bind=engine)
 
 API_VERSION = "0.1.0"
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Dependency
 
